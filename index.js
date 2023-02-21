29d2842f0632719c4124c1a8a057a367031ba481
Schlumen <lumen.haendler@web.de>
2023-02-21 21:04:25 +0100

    Prepare for querying mongoose models

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(morgan("combined", {stream: accessLogStream}));
 app.use(express.static("public"));
 // Turn on body-parser to read JSON from req-body
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let movies = JSON.parse(fs.readFileSync("movies.json"));
 let users = [
--- models.js
+++ models.js
@@ -1,27 +1,27 @@
 import mongoose from "mongoose";
 
 let movieSchema = mongoose.Schema({
-    Title: {type: String, required: true},
-    Description: {type: String, required: true},
-    Genre: {
-        Name: String,
-        Description: String
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    genre: {
+        name: String,
+        description: String
     },
-    Director: {
-        Name: String,
-        Bio: String
+    director: {
+        name: String,
+        bio: String
     },
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
+    actors: [String],
+    imageurl: String,
+    featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthdate: Date,
-    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: "Movie"}]
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    birthdate: Date,
+    favoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: "Movie"}]
 });
 
 let Movie = mongoose.model("Movie", movieSchema);
